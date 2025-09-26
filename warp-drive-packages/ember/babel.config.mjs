748900153fea1c7c94d2fbd9c89cc54fe5998d70
Chris Thoburn <cthoburn@auditboard.com>
2025-09-26 14:21:09 -0700

    chore: unpkg all the packages

--- warp-drive-packages/ember/babel.config.mjs
+++ warp-drive-packages/ember/babel.config.mjs
@@ -1,5 +1,21 @@
 import { macros } from '@warp-drive/core/build-config/babel-macros';
 
+import { buildMacros } from '@embroider/macros/babel';
+
+const IS_UNPKG_BUILD = Boolean(process.env.UNPKG_BUILD);
+let Macros = { babelMacros: [], templateMacros: [] };
+
+if (IS_UNPKG_BUILD) {
+  Macros = buildMacros({
+    configure: (config) => {
+      setConfig(config, {
+        compatWith: process.env.EMBER_DATA_FULL_COMPAT ? '99.0' : null,
+        forceMode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+      });
+    },
+  });
+}
+
 export default {
   plugins: [
     ...macros(),
@@ -12,9 +28,10 @@ export default {
       'babel-plugin-ember-template-compilation',
       {
         targetFormat: 'hbs',
-        transforms: [],
+        transforms: Macros.templateMacros,
       },
     ],
     ['module:decorator-transforms', { runtime: { import: 'decorator-transforms/runtime' } }],
+    ...Macros.babelMacros,
   ],
 };
