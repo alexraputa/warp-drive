5f50f613bcf50e7ad8da883dd307b63e2ffd32f5
Alex Raputa <o.raputa@gmail.com>
2026-03-01 22:48:01 +0200

    fix(unpublished-test-infra): update hooks typing

--- packages/unpublished-test-infra/src/test-support/test-helpers.ts
+++ packages/unpublished-test-infra/src/test-support/test-helpers.ts
@@ -1,11 +1,11 @@
 import { getTestMetadata, setupContext, SetupContextOptions, teardownContext, TestContext } from '@ember/test-helpers';
 
-import type { EmberHooks } from '@warp-drive/diagnostic';
+import type { Hooks as DiagnosticHooks } from '@warp-drive/diagnostic/-types';
 
-export function setupTest(hooks: EmberHooks<TestContext>, opts?: SetupContextOptions): void {
+export function setupTest(hooks: DiagnosticHooks<TestContext>, opts?: SetupContextOptions): void {
   const options = { waitForSettled: false, ...opts };
 
-  hooks.beforeEach(async function () {
+  hooks.beforeEach(async function (this: TestContext) {
     let testMetadata = getTestMetadata(this);
     testMetadata.framework = 'qunit';
 
